d3785f3ea0ef5427adcc72191013a8cadc36f451
Taha Shah <tahashah411@gmail.com>
2023-10-21 00:02:33 +0500

    color changes

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -17,8 +17,7 @@ export const Board = ({board, active=false}) => {
   return (
     <>
     {<div className={`grid grid-cols-3 w-max sm:border-4 x-sm:gap-2 p-2 x-sm:p-3 gap-1 border-2 sm:rounded-xl x-sm:border-1 rounded-lg border-black */}
-    ${active ? turn == 1 ? "bg-blue-400" : "bg-red-500" : "bg-neutral-900"}
-    ${boards[Math.floor(board/3)][board %3] === 1 ? "bg-blue-500" : boards[Math.floor(board/3)][board %3] === -1 ? "bg-red-600" : ""}`}>
+    ${boards[Math.floor(board/3)][board %3] === 1 ? "bg-blue-600" : boards[Math.floor(board/3)][board %3] === -1 ? "bg-red-900" : active ? turn == 1 ? "bg-blue-400" : "bg-red-500" : "bg-neutral-900"}`}>
       {superBoard[board].map((row, y) =>
       <>
         {row.map((col, x) => <Cell key={`board${board}-Cell${y*3+x}`} clickedCell={() => clickedCell(x, y)} val={superBoard[board][y][x]} won={boards[Math.floor(board/3)][board %3]}/>)}
--- src/Components/Cell.jsx
+++ src/Components/Cell.jsx
@@ -10,7 +10,7 @@ export const Cell = ({clickedCell, val, won=0}) => {
     ${val == 1 ? "hover:bg-blue-400" : val == -1 ? "hover:bg-red-500" : "hover:bg-neutral-100"}`}
     onClick={() => clickedCell()}>
     </div> :
-    <div className={`w-6 h-6 x-sm:w-8 x-sm:h-8 sm:w-10 sm:h-10 flex justify-center items-center
+    <div className={`w-6 h-6 rounded x-sm:w-8 x-sm:h-8 sm:w-10 sm:h-10 sm:rounded-lg x-sm:rounded-md flex justify-center items-center
     ${won === 1 ? "bg-blue-500" : "bg-red-600"}`}>
     </div>
     }
