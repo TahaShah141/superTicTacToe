ccee15f34573d2748dfbc8e966769e1c06068c14
Taha Shah <tahashah411@gmail.com>
2023-10-20 09:39:41 +0500

    Overflow error

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     setWinner(winnerTurn)
   }
   return (
-    <div className={`flex flex-col justify-center h-screen items-center ${winner == 1 ? "bg-blue-400" : winner == -1 ? "bg-red-500" : "bg-neutral-900"} gap-4 sm:gap-8`}>
+    <div className={`flex flex-col justify-center h-screen items-center ${winner == 1 ? "bg-blue-400" : winner == -1 ? "bg-red-500" : "bg-neutral-900"} gap-4 sm:gap-8 overflow-y-hidden`}>
     <h1 className={` font-mono text-2xl x-sm:text-3xl ${winner == 0 ? "text-white" : "text-black"}`}>{winner == 1 ? "Player 1 Won !!" : winner == -1 ? "Player 2 Won !!" : "Super Tic-Tac-Toe"}</h1>
     <GameContextProvider>
       <SuperBoard endGame={endGame}/>
