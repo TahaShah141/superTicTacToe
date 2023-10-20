6c19d5449b028e57bbe8b84b9555452dad700cc5
Taha Shah <tahashah411@gmail.com>
2023-10-20 23:51:07 +0500

    won board color changes

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -6,18 +6,24 @@ export const Board = ({board, active=false}) => {
   const {superBoard, boards, turn, dispatch} = useGameContext()
 
   const clickedCell = (x, y) => {
-    if (!active || boards[Math.floor(board/3)][board %3] != 0) return 
+    if (!active || boards[Math.floor(board/3)][board %3] !== 0) return 
     dispatch({type: "MOVE", payload: {board, y, x}})
   }
 
+  const playBoard = (<>
+    
+    </>)
+
   return (
-    <div className={`grid grid-cols-3 w-max sm:border-4 x-sm:gap-2 p-2 x-sm:p-3 gap-1 border-2 sm:rounded-xl x-sm:border-1 rounded-lg 
-    ${boards[Math.floor(board/3)][board %3] == 0 ? "border-black" : "border-black"}
-    ${boards[Math.floor(board/3)][board %3] == 1 ? "bg-blue-800" : boards[Math.floor(board/3)][board %3] == -1 ? "bg-red-800" : active ? turn == 1 ? "bg-blue-400" : "bg-red-500" : "bg-neutral-900"}`}>
+    <>
+    {<div className={`grid grid-cols-3 w-max sm:border-4 x-sm:gap-2 p-2 x-sm:p-3 gap-1 border-2 sm:rounded-xl x-sm:border-1 rounded-lg border-black */}
+    ${active ? turn == 1 ? "bg-blue-400" : "bg-red-500" : "bg-neutral-900"}
+    ${boards[Math.floor(board/3)][board %3] === 1 ? "bg-blue-500" : boards[Math.floor(board/3)][board %3] === -1 ? "bg-red-600" : ""}`}>
       {superBoard[board].map((row, y) =>
       <>
-        {row.map((col, x) => <Cell key={`board${board}-Cell${y*3+x}`} clickedCell={() => clickedCell(x, y)} val={superBoard[board][y][x]}/>)}
+        {row.map((col, x) => <Cell key={`board${board}-Cell${y*3+x}`} clickedCell={() => clickedCell(x, y)} val={superBoard[board][y][x]} won={boards[Math.floor(board/3)][board %3]}/>)}
       </>)}
-    </div>
+    </div>}
+    </>
   )
 }
--- src/Components/Cell.jsx
+++ src/Components/Cell.jsx
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from 'react'
 
-export const Cell = ({clickedCell, val}) => {
+export const Cell = ({clickedCell, val, won=0}) => {
+  
   return (
+    <> 
+    {won === 0 ? 
     <div className={`w-6 h-6 rounded x-sm:w-8 x-sm:h-8 sm:w-10 sm:h-10 sm:rounded-lg x-sm:rounded-md flex justify-center items-center border-2 x-sm:border-4 border-neutral-950  text-white
     ${val == 1 ? "bg-blue-500" : val == -1 ? "bg-red-600" : "bg-neutral-950"}
     ${val == 1 ? "hover:bg-blue-400" : val == -1 ? "hover:bg-red-500" : "hover:bg-neutral-100"}`}
     onClick={() => clickedCell()}>
+    </div> :
+    <div className={`w-6 h-6 x-sm:w-8 x-sm:h-8 sm:w-10 sm:h-10 flex justify-center items-center
+    ${won === 1 ? "bg-blue-500" : "bg-red-600"}`}>
     </div>
+    }
+    </>
   )
 }
